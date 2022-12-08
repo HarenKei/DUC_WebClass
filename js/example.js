f8756b8e4c1e8e2b3e5b80e6cd23a14863820ec6
Jeong Seung Min <harenkei@email.daelim.ac.kr>
2022-12-08 14:37:35 +0900

    이름을 클릭하고 변경하여 띄우는 예제 작성

--- js/example.js
+++ js/example.js
@@ -1,3 +1,20 @@
-const test = 10;
+const nameH1Element = document.querySelector('h1.inline');
+const connectNameElement = document.querySelector('span.red');
 
-console.log(test);
+const localName = localStorage.getItem('name');
+if (localName) {
+  nameH1Element.textContent = localName;
+  connectNameElement.textContent = localName;
+}
+
+
+nameH1Element.onclick = () => {
+  const inputName = prompt(`이름을 입력해주세요.`);
+  if (inputName) {
+    localStorage.setItem('name', inputName);
+    nameH1Element.textContent = inputName;
+    connectNameElement.textContent = inputName;
+  } else {
+    alert(`이름이 입력되지 않았습니다.`);
+  }
+};
