dfab2030e2392895b367de9b97aad0d600a22c9e
Jeong Seung Min <harenkei@email.daelim.ac.kr>
2022-12-08 14:59:35 +0900

    Modal 핸들링 실습

--- js/example.js
+++ js/example.js
@@ -1,20 +1,26 @@
 const nameH1Element = document.querySelector('h1.inline');
 const connectNameElement = document.querySelector('span.red');
+const inputModalElement = document.querySelector('dialog#inputModal');
+
+const setUserName = (name) => {
+  nameH1Element.textContent = name;
+  connectNameElement.textContent = name;
+};
 
 const localName = localStorage.getItem('name');
-if (localName) {
-  nameH1Element.textContent = localName;
-  connectNameElement.textContent = localName;
-}
-
+if (localName) setUserName(localName);
 
 nameH1Element.onclick = () => {
-  const inputName = prompt(`이름을 입력해주세요.`);
-  if (inputName) {
-    localStorage.setItem('name', inputName);
-    nameH1Element.textContent = inputName;
-    connectNameElement.textContent = inputName;
-  } else {
-    alert(`이름이 입력되지 않았습니다.`);
-  }
+  inputModalElement.showModal();
+  // const inputName = prompt(`이름을 입력해주세요.`);
+  // if (inputName) {
+  //   localStorage.setItem('name', inputName);
+  //   setUserName(inputName);
+  // } else {
+  //   alert(`이름이 입력되지 않았습니다.`);
+  // }
+};
+
+inputModalElement.onclick = (e) => {
+  if (e.target.nodeName === 'DIALOG') inputModalElement.close();
 };
