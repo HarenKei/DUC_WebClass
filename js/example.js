aa08d73c163df791f05b56d13c4af5c844d19e98
Jeong Seung Min <harenkei@email.daelim.ac.kr>
2022-12-08 16:27:35 +0900

    이메일/학번 유효성 검사

--- js/example.js
+++ js/example.js
@@ -1,14 +1,78 @@
 const nameH1Element = document.querySelector('h1.inline');
 const connectNameElement = document.querySelector('span.red');
 const inputModalElement = document.querySelector('dialog#inputModal');
+const modalSubmitButton = document.querySelector('button.modalSubmit');
+const studentNoSpanElement = document.querySelector('span.studentNo');
+const emailSpanElement = document.querySelector('span.email');
 
+//  아래 세개의 set 함수는 이름, 학번, 이메일이 표시되어야 할 HTML 요소의 값을 변경합니다.
 const setUserName = (name) => {
   nameH1Element.textContent = name;
   connectNameElement.textContent = name;
 };
 
-const localName = localStorage.getItem('name');
-if (localName) setUserName(localName);
+const setStudentNo = (stdNo) => {
+  studentNoSpanElement.textContent = stdNo;
+};
+
+const setUserEmail = (email) => { 
+  emailSpanElement.textContent = email;
+};
+
+const chkStudentNo = (stdNo) => {
+  if (stdNo.length != 9) {
+    return false;
+  } else {
+    return true;
+  }
+};
+
+const chkUserEmail = (email) => {
+  // eslint-disable-next-line max-len
+  // 이메일의 형식을 @, .의 존재 여부로 판별하는 함수입니다.
+  if (email.includes('@') && email.includes('.')) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const setUserInfomation = (formData) => {
+  // dialog를 통해 입력받은 값을 localStorage에 저장하고 화면에 나타냅니다.
+  for (const [key, value] of formData) {
+    localStorage.setItem(key, value);
+    if (key==='userName') {
+      setUserName(value);
+    } if (key === 'studentNo') {
+      setStudentNo(value);
+    } if (key === 'userEmail') {
+      setUserEmail(value);
+    }
+  }
+};
+
+const setLocalInfomation = () => {
+  //  새로고침이 되어도 저장된 localStorage의 값을 표시합니다.
+  const localName = localStorage.getItem('userName');
+  const localStudentNo = localStorage.getItem('studentNo');
+  const localUserEmail = localStorage.getItem('userEmail');
+
+  if (localName) setUserName(localName);
+  if (localStudentNo) setStudentNo(localStudentNo);
+  if (localUserEmail) setUserEmail(localUserEmail);
+};
+
+
+
+// const localName = localStorage.getItem('userName');
+// const localStudentNo = localStorage.getItem('studentNo');
+// const localUserEmail = localStorage.getItem('userEmail');
+
+// if (localName) setUserName(localName);
+// if (localStudentNo) setStudentNo(localStudentNo);
+// if (localUserEmail) setUserEmail(localUserEmail);
+
+setLocalInfomation();
 
 nameH1Element.onclick = () => {
   inputModalElement.showModal();
@@ -22,5 +86,28 @@ nameH1Element.onclick = () => {
 };
 
 inputModalElement.onclick = (e) => {
+  // 모달 창 이외의 부분을 클릭 시 모달 창 닫히는 함수
   if (e.target.nodeName === 'DIALOG') inputModalElement.close();
+};
+
+modalSubmitButton.onclick = (e) => {
+  // 모달 창 전송 버튼이 눌리면 form의 정보를 FormData 객체로 담는 함수.
+  const modalFormElement = document.querySelector('form.modalForm');
+  const formData = new FormData(modalFormElement);
+  for (const [key, value] of formData) {
+    localStorage.setItem(key, value);
+    if (key==='userName') {
+      setUserName(value);
+    } if (key === 'studentNo' && chkStudentNo()) {
+      setStudentNo(value);
+    } else {
+      alert(`잘못된 학번값.`);
+    }
+    if (key === 'userEmail' && chkUserEmail()) {
+      setUserEmail(value);
+    } else {
+      alert(`잘못된 이메일 형식.`);
+    }
+  }
+  inputModalElement.close();
 };
